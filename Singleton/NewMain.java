382da4b42c21daa9c8e488987a68186396f4b351
CHIKITIn19 <jimenezjeancarlos9@gmail.com>
2024-09-30 23:31:26 -0600

    Se agrega clases sin aplicar el patron de diseño prototype y singleton

--- Singleton/NewMain.java
+++ Singleton/NewMain.java
@@ -15,11 +15,24 @@ public class NewMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //Obtenemos la instancia de la clase (NO SE CREA, SIMPLEMENTE SE OBTIENE)
-        PatronSingleton pa = PatronSingleton.getInstance();
         
-        pa.Instancia(12);
-        pa.Message();
+        //Obtenemos la instancia de la clase (NO SE CREA, SIMPLEMENTE SE OBTIENE)
+        //PatronSingleton pa = PatronSingleton.getInstance();
+        
+        //pa.Instancia(12);
+        //pa.Message();
+        
+        
+        //A diferencia con el patron singleton, sin el patron podemos crear multiples instancias
+        SinPatron ejm1 = new SinPatron();
+        SinPatron ejm2 = new SinPatron();
+        
+        // Utilizar las instancias
+        ejm1.Instancia(12);
+        ejm1.Message();
+        
+        ejm2.Instancia(34);
+        ejm2.Message();
     }
     
 }
---
+++ Singleton/SinPatron.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Singleton;
+
+/**
+ *
+ * @author 9567
+ */
+public class SinPatron {
+    
+    public SinPatron(){
+        
+    }
+    
+    public void Message(){
+        System.out.println("Hola desde una instancia de mensaje");
+        
+    }
+    public void Instancia(int entero){
+        System.out.println("Hola desde una instancia de mensaje y numero entero " +entero);
+    }
+}
